93659663edd195b9e8aa841cb29b1648b113ae2c
Don Fitzgerald <don@sun.ac.za>
2021-11-25 17:06:08 +0200

    Updates to code

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ImageList from './components/ImageList'
 import axios from 'axios'
 import './components/styles.css'
 
 const App = () => {
     
-    const [pictures,setPictures] = useState({
-        data: '',
+    const [pictures,setPictures] = useState({data: '',})
+    
+    const [topics, setTopic] = useState('');
+
+    useEffect(async () => {
         
-    })    
+            const result = await axios.get('https://api.unsplash.com/topics', {        
+                headers: {Authorization: 'Client-ID v2fEICoN60I65AabhRYNGAh-MUiwEdUWLbApi402ykw'}
+            })
+
+            setTopic(result.data);
+                
+    }, []);
 
     const handleClick  = async(name) => {
         
         const data = await axios.get('https://api.unsplash.com/search/photos', {
-                     params: {query: name},
-                     headers: {Authorization: 'Client-ID v2fEICoN60I65AabhRYNGAh-MUiwEdUWLbApi402ykw'}            
+            params: {query: name},
+            headers: {Authorization: 'Client-ID v2fEICoN60I65AabhRYNGAh-MUiwEdUWLbApi402ykw'}            
         })
         
-        setPictures({
-            data: data,
-            
-        })       
+        setPictures({data: data,})       
     }
           
     return (
         <div>
             <h2 className="heading">Categories:</h2>
+            <div className= "btnsAndImages">
 
-            <div className= "btnsAndImages">          
+                {topics !== '' &&          
 
                 <div className ="btnCollective">
-                    <button className= "btn" onClick = {() => handleClick('Animation')} >Animation</button>
-                    <button className= "btn" onClick = {() => handleClick('Cartoon')} >Cartoon</button>
-                    <button className= "btn" onClick = {() => handleClick('Character Design')} >Character Design</button>
-                    <button className= "btn" onClick = {() => handleClick('Comedy')} >Comedy</button>
-                    <button className= "btn" onClick = {() => handleClick('Design')} >Design</button>
-                    <button className= "btn" onClick = {() => handleClick('DJing')} >DJing</button>
-                    <button className= "btn" onClick = {() => handleClick('Engineering')} >Engineering</button>
-                    <button className= "btn" onClick = {() => handleClick('Cars')} >Cars</button>
+
+                    {topics.map(topic => (
+                    <button className= "btn" onClick = {() => handleClick(topic.title)} >{topic.title}</button>
+                    ))}
+                    
                 </div>
+                }
 
                 <div>
-                    <ImageList foundImages={pictures.data} />                    
+                    <ImageList foundImages={pictures.data} /> 
                 </div>
-
             </div>         
-
         </div>
     )
 }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -6,7 +6,6 @@ const ImageList = (props) => {
     let imgs = '';
 
     if (props.foundImages){
-
         imgs = props.foundImages.data.results.map(img => {
             return <img className = "img" key={img.id} src={img.urls.regular} alt={img.alt_description} />
         })
@@ -16,21 +15,20 @@ const ImageList = (props) => {
         <div className = "images">
 
             <div>
-            {imgs [0]}
-            {imgs [1]}
-            {imgs [2]}
-            {imgs [3]}
-            {imgs [4]}
+            {imgs[0]}
+            {imgs[1]}
+            {imgs[2]}
+            {imgs[3]}
+            {imgs[4]}
             </div>
 
             <div>
-            {imgs [5]}
-            {imgs [6]}
-            {imgs [7]}
-            {imgs [8]}
-            {imgs [9]}
+            {imgs[5]}
+            {imgs[6]}
+            {imgs[7]}
+            {imgs[8]}
+            {imgs[9]}
             </div>
-
         </div>
     )
 }
